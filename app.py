6b78518933b138683e9d972a408e60e7a983d031
DjAxoo <monsdj.be@gmail.com>
2023-09-19 11:38:43 +0200

    Your commit message here

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title="Titanic Predict")
 
 if __name__ == '__main__':
     app.run(debug=True, port=3300)
